26c3db92626073610e551e0e06c3bb511b6cea88
conoro24 <60669583+conoro24@users.noreply.github.com>
2020-10-20 15:04:56 +0100

    Add files via upload

---
+++ LCA1test.java
@@ -0,0 +1,35 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class LCA1test {
+
+	
+	@Test
+	void test() {
+		
+		LCA1 tree = new LCA1(); 
+        tree.root = new Node(1); 
+        tree.root.left = new Node(2); 
+        tree.root.right = new Node(3); 
+        tree.root.left.left = new Node(4); 
+        tree.root.left.right = new Node(5); 
+        tree.root.right.left = new Node(6); 
+        tree.root.right.right = new Node(7);
+        
+		
+		assertEquals(2, tree.findLCA(4,5));
+		assertEquals(1, tree.findLCA(4,6));
+		assertEquals(1, tree.findLCA(3,4));
+		assertEquals(2, tree.findLCA(2,4));
+	}
+	@Test
+	void testEmpty() {
+		
+		LCA1 tree = new LCA1();
+		tree.root = null;
+		
+		assertEquals(-1, tree.findLCA(2, 1));
+	}
+
+}
