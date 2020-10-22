23248752b14a6c97487246acb7599cfb1ce5b3e6
conoro24 <60669583+conoro24@users.noreply.github.com>
2020-10-22 12:18:54 +0100

    Update LCA2.py

--- LCA2.py
+++ LCA2.py
@@ -1,44 +1,66 @@
 class Node: 
-    # Constructor to create a new node 
-    def __init__(self, data): 
-        self.data = data 
+    def __init__(self, key): 
+        self.key =  key 
         self.left = None
         self.right = None
   
-# Function to find LCA of a and b. The function assumes 
-# that both a and b are present in BST 
-def lca(root, a, b): 
-      
+# Finds the path from root node to given root of the tree. 
+# Stores the path in a list path[], returns true if path  
+# exists otherwise false 
+def findPath( root, path, k): 
+  
     if root is None: 
-        return None
+        return False
   
-    # If both a and b are smaller than root, then LCA lies in left 
-    if(root.data > a and root.data > b): 
-        return lca(root.left, a, b) 
+    # Store this node is path vector. The node will be 
+    # removed if not in path from root to k 
+    path.append(root.key) 
   
-    # If both a and b are greater than root, then LCA lies in right  
-    if(root.data < a and root.data < b): 
-        return lca(root.right, a, b) 
+    # See if the k is same as root's key 
+    if root.key == k : 
+        return True
   
-    return root 
+    # Check if k is found in left or right sub-tree 
+    if ((root.left != None and findPath(root.left, path, k)) or
+            (root.right!= None and findPath(root.right, path, k))): 
+        return True 
+  
+    # If not present in subtree rooted with root, remove 
+    # root from path and return False 
+       
+    path.pop() 
+    return False
+  
+# Returns LCA if node a , b are present in the given 
+# binary tre otherwise return -1 
+def lca(root, a, b): 
+  
+    path1 = [] 
+    path2 = [] 
+   
+    if (not findPath(root, path1, a) or not findPath(root, path2, b)): 
+        return -1 
+  
+    # Compare the paths to get the first different value 
+    i = 0 
+    while(i < len(path1) and i < len(path2)): 
+        if path1[i] != path2[i]: 
+            break
+        i += 1
+    return path1[i-1] 
   
 
-root = Node(6) 
-root.left = Node(4) 
-root.right = Node(8) 
-root.left.left = Node(3) 
+root = Node(1) 
+root.left = Node(2) 
+root.right = Node(3) 
+root.left.left = Node(4) 
 root.left.right = Node(5) 
-root.left.right.left = Node(1) 
-root.left.right.right = Node(10) 
+root.right.left = Node(6) 
+root.right.right = Node(7)
+root.right.right.right = Node(8) 
   
-a = 5 ; b = 1
-t = lca(root, a, b) 
-print ("LCA of %d and %d is %d" %(a, b, t.data)) 
-  
-a = 3 ; b = 10
-t = lca(root, a, b) 
-print ("LCA of %d and %d is %d" %(a, b , t.data)) 
-  
-a = 5 ; b = 8
-t = lca(root, a, b) 
-print ("LCA of %d and %d is %d" %(a, b, t.data))
+print "LCA(4, 5) = %d" %(lca(root, 4, 5,)) 
+print "LCA(4, 6) = %d" %(lca(root, 4, 6)) 
+print "LCA(3, 4) = %d" %(lca(root,3,4)) 
+print "LCA(2, 4) = %d" %(lca(root,2, 4))
+print "LCA(6, 8) = %d" %(lca(root,6, 8))
